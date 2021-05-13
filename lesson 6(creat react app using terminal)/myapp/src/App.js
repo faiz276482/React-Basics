c426b776d2cd2186721c371e3f483c7cce095751
Faiz Alam <faiz276482@gmail.com>
2021-05-13 22:00:15 +0530

    lesson 6.1 nesting components

--- lesson 6(creat react app using terminal)/myapp/src/App.js
+++ lesson 6(creat react app using terminal)/myapp/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
       <h1>My first react app</h1>
       <p>Welcome 😄</p>
+      <Ninjas/>
     </div>
   );
 }
---
+++ lesson 6(creat react app using terminal)/myapp/src/Ninjas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Ninjas = () => {
+    return(
+        <div>
+            <div>Name: Faiz</div>
+            <div>Age: 23</div>
+            <div>Belt: Brown</div>
+        </div>
+    );
+}
+
+export default Ninjas
