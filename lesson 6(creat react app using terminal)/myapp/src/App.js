4220d4b34fcf492476fb682f83d649342d8ad098
Faiz Alam <faiz276482@gmail.com>
2021-05-13 21:44:24 +0530

    created basic react app using npx create-react-app command

---
+++ lesson 6(creat react app using terminal)/myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first react app</h1>
+      <p>Welcome 😄</p>
+    </div>
+  );
+}
+
+export default App;
