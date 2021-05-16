10075a34e8d445116be616ce00100720974aa9b9
Faiz Alam <faiz276482@gmail.com>
2021-05-16 17:33:24 +0530

    Outputting lists of component  using map

--- lesson 6(creat react app using terminal)/myapp/src/App.js
+++ lesson 6(creat react app using terminal)/myapp/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
-import React from 'react';
+import React,{useState} from 'react';
 import Ninjas from './Ninjas';
 
 function App() {
+  let [ninjas,setNinjas]=useState([
+    {name:"Faiz",age:23,belt:"black",id:1},
+    {name:"Zaid",age:22,belt:"blue",id:2},
+    {name:"Damini",age:23,belt:"black",id:3},
+  ]);
+
+
   return (
     <div className="App">
       <h1>My first react app</h1>
       <p>Welcome 😄</p>
-      <Ninjas
+      {/* <Ninjas
         name="Faiz"
         age="28"
         belt="Black"
-      />
+      /> */}
+
+      <Ninjas ninjas={ninjas}/>
     </div>
   );
 }
--- lesson 6(creat react app using terminal)/myapp/src/Ninjas.jsx
+++ lesson 6(creat react app using terminal)/myapp/src/Ninjas.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 const Ninjas = (Props) => {
 
-    const {name,age,belt}=Props;
+    // const {name,age,belt}=Props;
     // console.log(name);
     // console.log(Props.age);
+
+    const {ninjas}=Props;
+    const ninjaList=ninjas.map(ninja=>{
+        return(
+            <div className="ninja" key={ninja.id}>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+            </div>
+        )
+    })
     return(
-        <div>
-            <div>Name: { name }</div>
-            <div>Age: { age }</div>
-            <div>Belt: { belt }</div>
+        // <div>
+        //     <div>Name: { name }</div>
+        //     <div>Age: { age }</div>
+        //     <div>Belt: { belt }</div>
+        // </div>
+        <div className="ninja-list">
+            {ninjaList}
         </div>
+        
     );
 }
 
