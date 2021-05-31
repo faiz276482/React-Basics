42ca03966119ed0cb7cc451a8f1fa97d05013754
Faiz Alam <faiz276482@gmail.com>
2021-05-31 21:59:15 +0530

    using image inside react component

--- react-redux-complete/navigation_bar_ex/src/components/Home.jsx
+++ react-redux-complete/navigation_bar_ex/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component {
 
@@ -20,8 +21,9 @@ class Home extends Component {
             posts.map(post=>{
                 return (
                 <div className="post card" key={post.id} style={{margin:100,marginTop:20,marginBottom:20}}>
+                    <img src={Pokeball} alt="A pokeball" />
                     <div className="card-content">
-                        <Link to={'/'+post.id}><span className="card-title">{post.title}</span></Link>
+                        <Link to={'/'+post.id}><span className="card-title blue-text">{post.title}</span></Link>
                         <p>{post.body}</p>
                     </div>
                 </div>)
@@ -30,10 +32,10 @@ class Home extends Component {
             <div className="center">No posts</div>
         )
         return (
-            <div className="conatiner">
+            <div className="conatiner home">
                  <h1 className="center">Home</h1>
                 {/*<p  className="center">lorem Ips dolor sit amet, consectetur adipiscing elit. Invento sollicitudin felis, sed do eiusmod tempor incididunt ut labore</p> */}
-                <div className="center">{postList}</div>
+                <div>{postList}</div>
             </div>
         )
     }
