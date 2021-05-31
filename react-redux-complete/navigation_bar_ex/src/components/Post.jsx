e1effafee1590266d388c43db4f2c4bb7b1949e7
Faiz Alam <faiz276482@gmail.com>
2021-06-01 00:40:07 +0530

    Fetched single post from local store dependeng upon the Router param value

--- react-redux-complete/navigation_bar_ex/src/components/Post.jsx
+++ react-redux-complete/navigation_bar_ex/src/components/Post.jsx
@@ -1,26 +1,29 @@
 import React,{Component} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
+import { connect } from 'react-redux'
 
 class Post extends Component {
-    state={
-        post:null
-    }
-    componentDidMount(){
-        console.log(this.props)
-        let id=this.props.match.params.post_id
-        axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
-            .then(res=>{
-                this.setState({post:res.data});
-                // console.log(res.data)
-            })
+    // state={
+    //     post:null
+    // }
+    // componentDidMount(){
+    //     console.log(this.props)
+    //     let id=this.props.match.params.post_id
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
+    //         .then(res=>{
+    //             this.setState({post:res.data});
+    //             // console.log(res.data)
+    //         })
         
-    }
+    // }
     render() {
-
-        const post=this.state.post?(
+        
+        const post=this.props.post?(
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p className="center">{this.state.post.body}</p>
+                {/* <h4 className="center">{this.state.post.title}</h4>
+                <p className="center">{this.state.post.body}</p> */}
+                <h4 className="center">{this.props.post.title}</h4>
+                <p className="center">{this.props.post.body}</p>
             </div>
         ):(
             <div className="center">Loading post...</div>
@@ -35,4 +38,11 @@ class Post extends Component {
     }
 }
 
-export default Post
+const mapStateToProps =(state,ownProps) =>{
+    let id=ownProps.match.params.post_id;
+    return {
+        post: state.posts.find(post => post.id===id)
+    }
+}
+
+export default connect(mapStateToProps)(Post)
